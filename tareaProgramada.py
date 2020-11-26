0342a6b52771d63cbee7dfc0101a1f41aef5e9fb
Esteban Sibaja <sibajajaja@gmail.com>
2020-11-25 21:14:53 -0600

    Update tareaProgramada.py

--- tareaProgramada.py
+++ tareaProgramada.py
@@ -1,3 +1,7 @@
+#Elaborado por : Diego Vega y Esteba Sibaja
+#Fecha de creacion: 18/11/2020 8:34pm
+#ultima modificacion: XXXX
+#version: 3.8.1
 from bs4 import BeautifulSoup
 from email.mime.multipart import MIMEMultipart
 from email.mime.base import MIMEBase
@@ -17,8 +21,12 @@ lista = []
 for i in info:
     if i != "":
         lista.append(i)
+<<<<<<< HEAD
 
 print(lista)
 def prueba():
     print('prueba mod 12')
     print('hola')
+=======
+#def crearMatriz(lista):
+>>>>>>> 32751b9bc8ddec052dd9974dce94e7a95e25d357
