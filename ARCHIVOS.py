049cb89f42225163ef7a9d381334de444833b2b1
Esteban Sibaja <sibajajaja@gmail.com>
2020-11-29 17:01:25 -0600

    Update ARCHIVOS.py

--- ARCHIVOS.py
+++ ARCHIVOS.py
@@ -2,4 +2,33 @@
 #fecha de creacion: 18/11/2020 8:34pm
 #ultima modificacion: XXXX
 #version: 3.8.1
-#importacion  de librerias
+#IMPORTACION DE LIBRERIAS
+import pickle
+#funciones
+def grabar(nombreArchivoGrabar,lista):
+    '''
+    funcion: crea o graba la lista en un archivo 
+    e: el nombre del archivo y a lista a guardar
+    s: prints
+    '''
+    try:
+        archivo = open(nombreArchivoGrabar,'wb')
+        pickle.dump(lista,archivo)
+        print('\n',nombreArchivoGrabar,'modificado excitosamente!\n')
+        archivo.close()
+    except:
+        print('error al grabar:',nombreArchivoGrabar)
+def leer(nombreArchivoLeer):
+    '''
+    funcion: leer el archivo 
+    e: nombre del archivo a leer
+    s: prints 
+    '''
+    lista = []
+    try:
+        archivo = open(nombreArchivoLeer,'rb')
+        lista = pickle.load(archivo)
+        archivo.close()
+    except:
+        print('error al leer:',nombreArchivoLeer)
+    return lista 
