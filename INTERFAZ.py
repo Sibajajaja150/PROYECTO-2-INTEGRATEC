20c22e704c615d04d2c66a35c5b4d1bf509ebfda
Esteban Sibaja <sibajajaja@gmail.com>
2020-12-01 00:38:16 -0600

    Update INTERFAZ.py

--- INTERFAZ.py
+++ INTERFAZ.py
@@ -8,26 +8,29 @@ from tkinter import *
 import pip
 from pip import *
 ###################INTERFAZ GRAFICA###################
+#VENTANA DEL PROGRAMA
 ventana = Tk() # crea una ventana
-ventana.title("ventana integratec")
-ventana.geometry("900x800")
-ventana.resizable(True, True)
+ventana.title("ventana integratec")#titulo de la ventana
+ventana.geometry("900x800")#tamaño de la ventana
+ventana.resizable(True, True)#opcionde modifacar el tamaño
+#PANEL 
+panel = Frame(ventana, bg='blue', width = 900, height = 800)#crea un panel dentro de la ventana donde se va a trabajar
+panel.place(x=0, y=0)#tamaño del panel
+#TEXTOS(LABELS)
+labelTitulo = Label(panel, text = "MENU INTEGRATEC", bg ='blue', fg = 'white',font = ('',25))#primer label con el titulo en el panel
+labelTitulo.place(x=300,y=50)
+#TEXTOS(LABELS)
+labelNumero = Label(panel, text = "ELIGE UNA OPCION PARA LLEVAR A CABO! ", bg = 'blue', fg = 'white',font = ('',15))#otro label con texto de opciones
+labelNumero.place(x=250, y=100)
+#BOTONES
+boton1 = Button(panel,text = '1)ESTUDIANTE POR SEDE',width=30, height = 2).place(x=350,y=150)
+boton2 = Button(panel,text = '2)ESTUDIANTES DE CARRERA POR SEDE',width=30, height = 2).place(x=350,y=200)
+boton3 = Button(panel,text = '3)CREAR MENTORES',width=30, height = 2).place(x=350,y=250)
+boton4 = Button(panel,text = '4)ASIGNAR MENTORES',width=30, height = 2).place(x=350,y=300)
+boton5 = Button(panel,text = '5)ACTUALIZAR ESTUDIANTE',width=30, height = 2).place(x=350,y=350)
+boton6 = Button(panel,text = '6)GENERAR REPORTE',width=30, height = 2).place(x=350,y=400)
+boton7 = Button(panel,text = '7)CREAR BASE DE DATOS EN EXCEL',width=30, height = 2).place(x=350,y=450)
+boton8 = Button(panel,text = '8)ENVIAR CORREO',width=30, height = 2).place(x=350,y=500)
+boton9 = Button(panel,text = '9)SALIR',width=30, height = 2).place(x=350,y=550)
 
-panel = Frame(ventana, bg='blue',width= 900, height=800)#crea un panel con caracterisiticas
-panel.place(x=0, y=0) #pocisiona el panel
-labelTitulo = Label(panel, text = "MENU INTEGRATEC", bg ='blue', fg = 'white',font = ('',18))#primer label
-labelTitulo.place(x=350,y=100)
-labelNumero = Label(panel, text = "INGRESE EL NUMERO", bg = 'blue', fg = 'white',font = ('',18))#otro label
-labelNumero.place(x=75, y=150)
-panel = Frame(ventana, bg='gray18', width = 900, height = 800)
-panel.place(x=0, y=0)
-boton1 = Button(ventana,text = '1)ESTUDIANTE POR SEDE',width=30, height = 2).place(x=350,y=150)
-boton2 = Button(ventana,text = '2)ESTUDIANTES DE CARRERA POR SEDE',width=30, height = 2).place(x=350,y=200)
-boton3 = Button(ventana,text = '3)CREAR MENTORES',width=30, height = 2).place(x=350,y=250)
-boton4 = Button(ventana,text = '4)ASIGNAR MENTORES',width=30, height = 2).place(x=350,y=300)
-boton5 = Button(ventana,text = '5)ACTUALIZAR ESTUDIANTE',width=30, height = 2).place(x=350,y=350)
-boton6 = Button(ventana,text = '6)GENERAR REPORTE',width=30, height = 2).place(x=350,y=400)
-boton7 = Button(ventana,text = '7)CREAR BASE DE DATOS EN EXCEL',width=30, height = 2).place(x=350,y=450)
-boton8 = Button(ventana,text = '8)ENVIAR CORREO',width=30, height = 2).place(x=350,y=500)
-boton9 = Button(ventana,text = '9)SALIR',width=30, height = 2).place(x=350,y=550)
-ventana.mainloop()
+ventana.mainloop()#LLAMADA PRINCIPAL
