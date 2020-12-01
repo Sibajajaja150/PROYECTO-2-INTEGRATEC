6bd3a8ff404d233ada7f5897c198ee39a234dd9f
Esteban Sibaja <sibajajaja@gmail.com>
2020-11-30 22:47:24 -0600

    Update INTERFAZ.py

--- INTERFAZ.py
+++ INTERFAZ.py
@@ -7,23 +7,13 @@ import tkinter
 from tkinter import *
 import pip
 from pip import *
-from 
+
 ###################INTERFAZ GRAFICA###################
 ventana = Tk() # crea una ventana
 ventana.title("ventana principal")
+ventana.iconbitmap(r'C:\Users\esteb\OneDrive\Imágenes\TEC\iconoPython.png')
 ventana.geometry("900x800")
 ventana.resizable(FALSE, FALSE)
-imagenIntegratec = ImageTK.PhotoImage(Image.open(r'C:\Users\esteb\OneDrive\Imágenes\TEC\integratec.png'))
-image1 = Image.tk.PhotoImage(imagenIntegratec)
-w = image1.width()
-h = image1.height()
-ventana.geometry('%dx%d+0+0' % (w,h))
-labelText = StringVar()
-labelText.set("Welcome !!!!")
-label1 = Label(ventana, image=image1, textvariable=labelText,
-                  font=("Times New Roman", 24),
-                  justify=CENTER, height=4, fg="blue")
-label1.pack()
 panel = Frame(ventana, bg='blue', width = 900, height = 800)
 panel.place(x=0, y=0)
 ventana.mainloop() 
