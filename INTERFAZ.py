9bccf439612f18333e277d36e1b975d8c8392944
Esteban Sibaja <sibajajaja@gmail.com>
2020-12-02 02:13:32 -0600

    Update INTERFAZ.py

--- INTERFAZ.py
+++ INTERFAZ.py
@@ -18,16 +18,42 @@ ventana.resizable(True, True)#opcionde modifacar el tamaño
 panel = Frame(ventana, bg='RoyalBlue3', width = 900, height = 800)#crea un panel dentro de la ventana donde se va a trabajar
 panel.place(x=0, y=0)#tamaño del panel
 #TEXTOS(LABELS)
-labelTitulo = Label(panel, text = "MENU INTEGRATEC", bg ='blue', fg = 'white',font = ('',25))#primer label con el titulo en el panel
+labelTitulo = Label(panel, text = "MENU INTEGRATEC", bg ='snow', fg = 'gray10',font = ('',25))#primer label con el titulo en el panel
 labelTitulo.place(x=300,y=50)
 #TEXTOS(LABELS)
-labelNumero = Label(panel, text = "ELIGE UNA OPCION PARA LLEVAR A CABO! ", bg = 'blue', fg = 'white',font = ('',15))#otro label con texto de opciones
+labelNumero = Label(panel, text = "ELIGE UNA OPCION PARA LLEVAR A CABO! ", bg = 'snow', fg = 'gray10',font = ('',15))#otro label con texto de opciones
 labelNumero.place(x=250, y=100)
 #FUNCIONES DE CADA BOTON
 def boton1():
-    print('hola')
+    panel1 = Frame(panel, bg='RoyalBlue3', width = 900, height = 800)
+    panel1.place(x=0, y=0)
+    labelTitulo1 = Label(panel1, text = "ESTUDIANTES POR SEDE", bg ='snow', fg = 'gray10',font = ('',20))
+    labelTitulo1.place(x=300,y=25)
+    ctcc = Label(panel1, text = 'CTCC', bg = 'snow', fg = 'gray10', font = ('',15))
+    ctcc.place(x=80,y=100)
+    ctsc = Label(panel1, text = 'CTSC', bg = 'snow', fg = 'gray10', font = ('',15))
+    ctsc.place(x=80,y=150)
+    ctsj = Label(panel1, text = 'CTSJ', bg = 'snow', fg = 'gray10', font = ('',15))
+    ctsj.place(x=80,y=200)
+    caa = Label(panel1, text = 'CAA', bg = 'snow', fg = 'gray10', font = ('',15))
+    caa.place(x=80,y=250)
+    cal = Label(panel1, text = 'CAL', bg = 'snow', fg = 'gray10', font = ('',15))
+    cal.place(x=80,y=300)
+    inputCTCC = Text(panel, width = 30, height = 2)#pone un cuadro de texto donde se pueda escrbir
+    inputCTCC.place(x = 150, y = 100)
+    inputCTSC = Text(panel, width = 30, height = 2)
+    inputCTSC.place(x = 150, y = 150)
+    inputCTSJ = Text(panel, width = 30, height = 2)
+    inputCTSJ.place(x = 150, y = 200)
+    inputCAA = Text(panel, width = 30, height = 2)
+    inputCAA.place(x = 150, y = 250)
+    inputCAL = Text(panel, width = 30, height = 2)
+    inputCAL.place(x = 150, y = 300)
+    estudiantesPorSede(inputCTCC,inputCTSC,inputCTSJ,inputCAA,inputCAL)
+def boton2():
+    return''
 #BOTONES
-boton1 = Button(panel,text = '1)ESTUDIANTE POR SEDE',width=30, height = 2, command = boton1()).place(x=350,y=150)
+boton1 = Button(panel,text = '1)ESTUDIANTE POR SEDE',width=30, height = 2, command = boton1).place(x=350,y=150)
 boton2 = Button(panel,text = '2)ESTUDIANTES DE CARRERA POR SEDE',width=30, height = 2).place(x=350,y=200)
 boton3 = Button(panel,text = '3)CREAR MENTORES',width=30, height = 2).place(x=350,y=250)
 boton4 = Button(panel,text = '4)ASIGNAR MENTORES',width=30, height = 2).place(x=350,y=300)
