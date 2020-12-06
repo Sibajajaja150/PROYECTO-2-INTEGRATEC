a0122a872d6a4033bcabeade48b192de9b0c0d5c
Godie-232 <vega.diego02@gmail.com>
2020-12-05 18:09:04 -0600

    Update MAINPR2.py

--- MAINPR2.py
+++ MAINPR2.py
@@ -17,10 +17,11 @@ import csv
 archivo = ""
 listaCE = []
 matrizSede = []
+#Se extrae la informacion de la pagina del tec
 url = "https://www.tec.ac.cr/carreras"
 page = requests.get(url)
 soup = BeautifulSoup(page.content, "html.parser")
-cuadros = soup.find_all ("div", class_ = "group")
+cuadros = soup.find_all ("div", class_ = "group") #se divide la info segun el codigo html 
 info = ""
 for i in cuadros:
     info += i.text
@@ -28,7 +29,7 @@ info = info.split("\n")
 lista = []
 for i in info:
     if i != "":
-        lista.append(i)
+        lista.append(i) #Se crea una lista con las sedes y las carreras
 def crearListaSJ(lista):
     listaSJ = []
     for i in lista[1:]:
@@ -110,7 +111,8 @@ def validarEstudiantesPorSede(a,b,c,d,e):
     global matrizSede
     if isNum(a) and isNum(b) and isNum(c) and isNum(d) and isNum(e):
         matrizSede = estudiantesPorSede(int(a), int(b), int(c), int(d), int(e))
-        return ''
+        print (matrizSede)
+        return ""
 def crearListaCarreras(lista):
     listaN = [] 
     k = lista[1].keys()
@@ -185,9 +187,6 @@ def getCorreo(lista):
     for i in lista:
         contador = 1
         correo =  getNameLastName(i[1])[0][0][:contador] + getNameLastName(i[1])[1][0] + "@estudiantec.cr"
-        #while correo not in correos:
-            #contador += 1
-            #correo =  getNameLastName(i[1])[0][0][:contador] + getNameLastName(i[1])[1][0] + "@estudiantec.cr"
         correos += [correo]
         i[-1] = correo
     return lista
@@ -199,7 +198,7 @@ def actualizarEstudiante(carnet, nombre, telefono, correo, dicc):
     dicc[carnet][5] = correo
     print(dicc[carnet])
     return ""
-validarEstudiantesPorSede('2', '4', '11', '18', '25')
+print(validarEstudiantesPorSede('2', '4', '11', '18', '25'))
 listaCE = [["CTCC", crearLista(crearListaCarreras(matrizSede[0]), crearListaEstudiantes(matrizSede[0]))], ["CTLSC", crearLista(crearListaCarreras(matrizSede[1]), crearListaEstudiantes(matrizSede[1]))], ["CTLSJ", crearLista(crearListaCarreras(matrizSede[2]), crearListaEstudiantes(matrizSede[2]))], ["CAA", crearLista(crearListaCarreras(matrizSede[3]), crearListaEstudiantes(matrizSede[3]))], ["CAL", crearLista(crearListaCarreras(matrizSede[4]), crearListaEstudiantes(matrizSede[4]))]]
 #print(listaCE)
 #diccEstudiantes = {listaCE[0][0]:listaCE[0][1], listaCE[1][0]:listaCE[1][1], listaCE[2][0]:listaCE[2][1], listaCE[3][0]:listaCE[3][1], listaCE[4][0]:listaCE[4][1]}
@@ -368,7 +367,7 @@ def enviarCorreos(correo):
     mensaje["From"] = "diegoesteban42069@gmail.com"
     usuario = correo
     mensaje["To"] = usuario
-    mensaje["Subject"] = "Prueba"
+    mensaje["Subject"] = "Reporte de Estudiantes IntegraTEC"
     adjunto = MIMEBase("application", "octect-stream")
     adjunto.set_payload(open(archivo,"rb").read())
     adjunto.add_header("content-Disposition", "attachment; filename = 'BDIntegraTEC.csv'")
@@ -390,7 +389,7 @@ def crearExcel(lista1, lista2):
     for i in lista2:
         file.write(i[0]+" "+' , '+i[1]+" "+' , '" 0 "+" "+' , '+i[2]+" "+' , '+i[3]+" "+' , '+" 0 "+" "+' , '+i[4] + '\n')
     return ""
-a = listaEstudiantes(listaCE)
-asignarMentores(a, listaMentores(listaCE))
-crearExcel(getCorreo(a), getCorreo(listaMentores(listaCE)))
-#enviarCorreos("sibajajaja@gmail.com")
+a = getCorreo(listaEstudiantes(listaCE))
+b = asignarMentores(a, getCorreo(listaMentores(listaCE)))
+#crearExcel(getCorreo(a), getCorreo(listaMentores(listaCE)))
+#enviarCorreos("vega.diego02@gmail.com")
